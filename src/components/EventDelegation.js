c8bdd734ee133404718df3ccbdbdef7d46b3b7c3
pwsd21 <pawansachdeva1998@gmail.com>
2024-02-18 02:32:42 +0530

    Event Delegation

---
+++ src/components/EventDelegation.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const name = ["pawan", "jatin", "Gautam"];
+
+const EventDelegation = () => {
+  const handleClick = (e) => {
+    if (e.target.tagName === "LI") {
+      console.log(e.target.textContent);
+    }
+  };
+  return (
+    <div className="main" onClick={handleClick}>
+      <h1>Ram Ram Bhai</h1>
+      {name.map((item, index) => (
+        <ul>
+          <li key={index}>{item}</li>
+        </ul>
+      ))}
+    </div>
+  );
+};
+
+export default EventDelegation;
